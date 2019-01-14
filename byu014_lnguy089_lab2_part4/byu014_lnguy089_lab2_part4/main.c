7a75b34fdf84e801e695a11f1b0eedb24d168d38
byu014 <byu014@ucr.edu>
2019-01-13 22:22:17 -0800

    even more

--- byu014_lnguy089_lab2_part4/byu014_lnguy089_lab2_part4/main.c
+++ byu014_lnguy089_lab2_part4/byu014_lnguy089_lab2_part4/main.c
@@ -5,7 +5,13 @@
  * Author : Sky
  */ 
 
-
+/*Bailey Yu, byu014@ucr.edu
+ *Liem Nguyen, lnguy089@ucr.edu
+ *Lab section: 22
+ *Assignment: Lab 2 Exercise 4
+ *I acknoledge all content contained herein, excluding template
+ *or example code, is my own original work.
+ */
 
 #include <avr/io.h>
 #include <stdlib.h>
