340fd2f80fc7f6f5f74d5739544ba7c997c71421
byu014 <byu014@ucr.edu>
2019-01-15 13:28:36 -0800

    finished lab3 part 1

--- byu014_lnguy089_lab3_part1/byu014_lnguy089_lab3_part1/main.c
+++ byu014_lnguy089_lab3_part1/byu014_lnguy089_lab3_part1/main.c
@@ -8,12 +8,19 @@
 /*Bailey Yu, byu014@ucr.edu
  *Liem Nguyen, lnguy089@ucr.edu
  *Lab section: 22
- *Assignment: Lab 2 Exercise 2
+ *Assignment: Lab 3 Exercise 1
  *I acknowledge all content contained herein, excluding template
  *or example code, is my own original work.
  */
 
 #include <avr/io.h>
+
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
 
 
 int main(void)
@@ -22,14 +29,31 @@ int main(void)
 	DDRA = 0x00; PORTA = 0x00;
 	DDRB = 0x00; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
-	unsigned char counter = 0;
+	unsigned char tempA = 0x00;
+	unsigned char tempB = 0x00;
+	unsigned char tempC = 0x00;
+	//unsigned char counter = 0;
 	unsigned char i = 0;
     while (1) 
     {
+		tempA = PINA;
+		tempB = PINB;
+		tempC = 0;
+		//counter = 0;
 		for(i = 0; i < 8; ++i)
 		{
-			if(GetBit)	
+			if(GetBit(tempA, i))
+			{
+				tempC += 1;
+			}	
+			if(GetBit(tempB,i))
+			{
+				tempC += 1;	
+			}
+			
 		}
+		//tempC = counter;
+		PORTC = tempC;
     }
 }
 
